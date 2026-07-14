07c19d9150457f9167ae9166dad0248c15ef6ed4
Prince Barnwal <princebarnwal274206@gmail.com>
2026-07-14 21:15:30 +0530

    Time: 1 ms (98.7%), Space: 47 MB (96.83%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,13 +1,13 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int totalSum = 0;
+        int rightSum = 0;
         for(int num : nums){
-            totalSum += num;
+            rightSum += num;
         }
 
         int leftSum = 0;
         for(int i=0; i < nums.length; i++){
-            int rightSum = totalSum - leftSum - nums[i];
+             rightSum -= nums[i];
             if(rightSum ==  leftSum ){
                 return i;
             }
